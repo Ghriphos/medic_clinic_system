c1fd0ecb6130c7d6eb64442b9ab35b7201dd99b9
unknown <ctiguarulhos@ifsp.edu.br>
2022-10-13 14:42:51 -0300

    att pequena

---
+++ model/classes/clinic.php
@@ -0,0 +1,17 @@
+<?php
+//get = pessoa pode ver     set = pessoa pode alterar
+//baixando php get e setters e php debbug
+class clinic{
+    private $cod_clinic;
+    private $name;
+    private $cnpj;
+    private $email;
+    private $street;
+    private $street_number;
+    private $street_complement;
+    private $district;
+    private $phone;
+    private $open_hour;
+    private $close_hour;
+}
+?>
---
+++ model/classes/person.php
@@ -0,0 +1,14 @@
+<?php
+class person{
+    private $cod_person;
+    private $name;
+    private $phone;
+    private $street;
+    private $idade;
+    private $cpf;
+    private $datanasct;
+    private $prontuario;
+    private $cttemerg;
+    private $estadocivil;
+}
+?>
